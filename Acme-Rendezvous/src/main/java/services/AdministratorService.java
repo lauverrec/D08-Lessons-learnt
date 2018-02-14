29aafbd95d7715b3093f3a31c683e322175f89b3
espisepi <sepinaco@gmail.com>
2018-02-14 21:54:17 +0100

    feat: CRUD de repositorio y servicio de administrator

---
+++ Acme-Rendezvous/src/main/java/services/AdministratorService.java
@@ -0,0 +1,86 @@
+
+package services;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import repositories.AdministratorRepository;
+import security.Authority;
+import security.LoginService;
+import security.UserAccount;
+import domain.Administrator;
+
+@Service
+@Transactional
+public class AdministratorService {
+
+	// Managed repository -----------------------------------------------------
+	@Autowired
+	private AdministratorRepository	administratorRepository;
+
+
+	// Simple CRUD methods------------------------------------------------
+	public Collection<Administrator> findAll() {
+		Collection<Administrator> result;
+		result = this.administratorRepository.findAll();
+		Assert.notNull(result);
+		return result;
+	}
+
+	public Administrator findOne(final int administratorId) {
+		Assert.isTrue(administratorId != 0);
+		Administrator result;
+		result = this.administratorRepository.findOne(administratorId);
+		return result;
+	}
+
+	public Administrator save(final Administrator administrator) {
+
+		Assert.notNull(administrator);
+		final Administrator result;
+		final Md5PasswordEncoder encoder;
+		final String passwordHash;
+
+		if (administrator.getId() == 0) {
+			final String password = administrator.getUserAccount().getPassword();
+			encoder = new Md5PasswordEncoder();
+			passwordHash = encoder.encodePassword(password, null);
+			administrator.getUserAccount().setPassword(passwordHash);
+		}
+		result = this.administratorRepository.save(administrator);
+
+		Assert.notNull(result);
+
+		return result;
+	}
+
+	// Other business methods------------------------------------------------------
+	public Administrator findByPrincipal() {
+		Administrator result;
+		UserAccount userAccount;
+		userAccount = LoginService.getPrincipal();
+		Assert.notNull(userAccount);
+		result = this.administratorRepository.findByUserAccountId(userAccount.getId());
+		return result;
+	}
+
+	public void checkPrincipal() {
+
+		final UserAccount userAccount = LoginService.getPrincipal();
+		Assert.notNull(userAccount);
+
+		final Collection<Authority> authorities = userAccount.getAuthorities();
+		Assert.notNull(authorities);
+
+		final Authority auth = new Authority();
+		auth.setAuthority(Authority.ADMINISTRATOR);
+
+		Assert.isTrue(authorities.contains(auth));
+	}
+
+}
