548b8553bc983dcff5fa538f0e6591fbf849abd2
joaquinperez94 <blogdeaula78@gmail.com>
2018-02-15 12:52:20 +0100

    Servicios, repositorios, controladores (a medias)

--- Acme-Rendezvous/src/main/java/repositories/AnnouncementRepository.java
+++ Acme-Rendezvous/src/main/java/repositories/AnnouncementRepository.java
@@ -1,6 +1,17 @@
+
 package repositories;
 
+import java.util.Collection;
 
-public interface AnnouncementRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import domain.Announcement;
+
+@Repository
+public interface AnnouncementRepository extends JpaRepository<Announcement, Integer> {
+
+	@Query("select a from Announcement a where a.rendezvouse.id=?1")
+	Collection<Announcement> findAnnouncementByRendezvousId(int rendezvouseId);
 }
