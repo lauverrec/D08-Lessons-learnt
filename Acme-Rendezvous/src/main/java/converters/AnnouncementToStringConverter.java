62e587dcb51237989934626acc650f151236740e
lauverrec <lauraverarec@gmail.com>
2018-02-15 12:11:37 +0100

    Convertidores sin acabar
    Faltan algunos repositorios

---
+++ Acme-Rendezvous/src/main/java/converters/AnnouncementToStringConverter.java
@@ -0,0 +1,25 @@
+package converters;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import domain.Announcement;
+
+
+@Component
+@Transactional
+public class AnnouncementToStringConverter implements Converter<Announcement, String>{
+	
+	@Override
+	public String convert(Announcement announcement) {
+		String result;
+
+		if (announcement == null)
+			result = null;
+		else
+			result = String.valueOf(announcement.getId());
+		return result;
+	}
+
+}
